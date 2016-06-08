2069074173de12e213bc8a0577e648feb6933226
Chad Granum <exodist7@gmail.com>
2016-06-08 09:02:57 -0700

    Progress

--- js/shBrushTAP.js
+++ js/shBrushTAP.js
@@ -14,20 +14,22 @@
         // Contributed by Chad Granum
         this.regexList = [
             // Plan
-            { regex: new RegExp('^1..\\d+', 'gm'), css: 'plain bold italic' },
+            { regex: new RegExp('^\\s*1..\\d+', 'gm'), css: 'plain bold italic' },
 
             // Match ok, not ok, and with test numbers
-            { regex: new RegExp('^ok( \\d+)?', 'gm'),     css: 'keyword' },
-            { regex: new RegExp('^not ok( \\d+)?', 'gm'), css: 'color3 bold' },
+            { regex: new RegExp('^\\s*ok( \\d+)?', 'gm'),     css: 'keyword' },
+            { regex: new RegExp('^\\s*not ok( \\d+)?', 'gm'), css: 'color3 bold' },
+            { regex: new RegExp('^\\s*}', 'gm'), css: 'plain bold' },
+            { regex: new RegExp('(?!(not )?ok .*){$', 'gm'), css: 'plain bold' },
 
             // Directives
             { regex: new RegExp('(?!^\\s*)#.*$', 'gm'), css: 'variable bold' },
 
             // Diagnostics
-            { regex: new RegExp('^#.*$', 'gm'), css: 'comments bold' },
+            { regex: new RegExp('^\\s*#.*$', 'gm'), css: 'comments bold' },
 
             // comments
-            { regex: new RegExp('^(?!(not )?ok)[^1].*$', 'gm'), css: 'comments' },
+            { regex: new RegExp('^(?!\\s*(not )?ok)[^1].*$', 'gm'), css: 'comments' },
 
             // Quoted items in messages
 			{ regex: SyntaxHighlighter.regexLib.doubleQuotedString, css: 'string' },
