6bccc5848e65a3d3899173584747a078c35214c5
Chad Granum <exodist7@gmail.com>
2017-04-07 14:35:30 -0700

    Add test2 formatter (needs tests and docs)
    This depends ont he event_facets branch of Test2

--- lib/Test2/Util/Term.pm
+++ lib/Test2/Util/Term.pm
@@ -3,10 +3,35 @@ use strict;
 use warnings;
 
 use Term::Table::Util qw/term_size USE_GCS USE_TERM_READKEY uni_length/;
+use Test2::Util qw/IS_WIN32/;
 
 our $VERSION = '0.000071';
 
 use Importer Importer => 'import';
-our @EXPORT_OK = qw/term_size USE_GCS USE_TERM_READKEY uni_length/;
+our @EXPORT_OK = qw/term_size USE_GCS USE_TERM_READKEY uni_length USE_ANSI_COLOR/;
+
+{
+    my $use = 0;
+    local ($@, $!);
+
+    if (eval { require Term::ANSIColor }) {
+        if (IS_WIN32) {
+            if (eval { require Win32::Console::ANSI }) {
+                Win32::Console::ANSI->import();
+                $use = 1;
+            }
+        }
+        else {
+            $use = 1;
+        }
+    }
+
+    if ($use) {
+        *USE_ANSI_COLOR = sub() { 1 };
+    }
+    else {
+        *USE_ANSI_COLOR = sub() { 0 };
+    }
+}
 
 1;
